8387ced8113901344cf486122d7a8707c1876639
Anish Baraily <An2001.baraily@gmail.com>
2024-03-19 21:41:49 +0545

    api work

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,9 +1,16 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../Config/Gemini";
 
-const Context = createContext();
+export const Context = createContext();
 
 const ContextProvider = (props) => {
+
+    const [input,setInput] = useState("")
+    const [recentPrompt,setRecentPrompt] = useState("")
+    const [prevPrompt,setPrevPrompt] = useState([])
+    const [showResult,setShowResult] = useState(false)
+    const [loading,setLoading] = useState(false)
+    const [resultData,setResultData] = useState("")
 
     const onSent = async (prompt) => {
         await runChat(prompt)
@@ -12,12 +19,21 @@ const ContextProvider = (props) => {
     onSent("what is react js")
 
     const contextValue = {
-
+        prevPrompt,
+        setPrevPrompt,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
     }
     
     return(
         <Context.Provider value={contextValue}>
-        {props.children}
+            {props.children}
         </Context.Provider>
     )
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import ContextProvider from './Context/Context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ContextProvider>
-    <App />
+    <App/>
   </ContextProvider>,
 )
